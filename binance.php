ae54032b6df7e99f3a3312b4028e1439d6dd558c
saifulabroriy <saifulabroriy2000@gmail.com>
2021-11-30 09:06:30 +0700

    initial commit

---
+++ binance.php
@@ -0,0 +1,134 @@
+<?php
+    // Include file module telegram 
+    include_once "telegram.php";
+
+    // Mendapatkan state bot telegram
+    $state = $_GET["state"];
+
+    // Fungsi mendapatkan data di return menjadi JSON dari URL
+    function getData($url){
+        // persiapkan curl
+        $curl = curl_init(); 
+
+        // Set Opsi pada curl
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+
+        // eksekusi dan dimasukkan ke markets
+        $res = curl_exec($curl); 
+
+        // Error Handler
+        if (curl_errno($curl)) {
+            print "Error: " . curl_error($curl);
+        } else {
+            // Tutup curl
+            curl_close($curl);
+
+            return json_decode($res);
+        }
+    }
+
+    // Mendapatkan symbols
+    $url = "https://api.binance.me/api/v3/exchangeInfo";
+    $ex_infos = getData($url);
+    $symbols = $ex_infos->symbols;
+
+    // Mendapatkan data ticker
+    $url = "https://api.binance.me/api/v3/ticker/24hr";
+    $ticker = getData($url);
+
+    // Jika page tidak diisi maka page = 1
+    $page = !isset($_GET['page']) ? 1 : $_GET['page'];
+    
+    // limit
+    $limit = 100;
+
+    // posisi atau cursor
+    $offset = ($page - 1) * $limit;
+
+    // potong array asli menjadi array yang dilimit
+    $limited_symbols = array_splice($symbols, $offset, $limit); // splice them according to offset and limit
+    $limited_ticker = array_splice($ticker, $offset, $limit); // splice them according to offset and limit
+
+    // array untuk tempat aset lowest dan highest
+    $low_asset = array();
+    $high_asset = array();
+
+    // Cek lowest dan highest
+    for($i = 0; $i <= count($limited_ticker) - 1; $i++) {
+        $koin = $limited_symbols[$i]->baseAsset;
+        $pair = $limited_symbols[$i]->quoteAsset;
+        $last = $limited_ticker[$i]->lastPrice;
+        $low = $limited_ticker[$i]->lowPrice;
+        $high = $limited_ticker[$i]->highPrice;
+        $lowest = $last - $low;
+        $highest = $high - $last;
+
+        // Cek price
+        if($lowest <= 0){
+            array_push($low_asset, "$koin/$pair");
+        }
+
+        if($highest <= 0){
+            array_push($high_asset, "$koin/$pair");
+        }
+    }
+    
+    $num = 1;
+    for($i = 0; $i <= count($limited_ticker) - 1; $i++) {
+        // membongkar menjadi beberapa variable agar mudah
+        $koin = $limited_symbols[$i]->baseAsset;
+        $pair = $limited_symbols[$i]->quoteAsset;
+        $last = $limited_ticker[$i]->lastPrice;
+        $low = $limited_ticker[$i]->lowPrice;
+        $high = $limited_ticker[$i]->highPrice;
+        $last_formatted = number_format($last, 3, ",", ".");
+        $low_formatted = number_format($low, 3, ",", ".");
+        $high_formatted = number_format($high, 3, ",", ".");
+        $lowest = number_format($last - $low, 3, ",", ".");
+        $highest = number_format($high - $last, 3, ",", ".");
+
+        // Membuat baris tabel
+        echo "<tr class=\"table__row\">";
+        echo "<td class=\"table__item table__item--number\">$num</td>";
+        echo "<td class=\"table__item table__item--coin\">$koin</td>";
+        echo "<td class=\"table__item table__item--pair\">$pair</td>";
+        echo "<td class=\"table__item table__item--last\">$last_formatted</td>";
+        echo "<td class=\"table__item table__item--low\">$low_formatted</td>";
+        echo "<td class=\"table__item table__item--high\">$high_formatted</td>";
+        echo "<td class=\"table__item table__item--lowest\">$lowest</td>";
+        echo "<td class=\"table__item table__item--highest\">$highest</td>";
+        echo "</tr>";
+
+        $num++;
+    }
+
+    // Jika state yang dikirim true
+    if ($state == "true"){
+        // Mendapatkan timestamp dari mikrotime bingbon
+        $waktu = round($ex_infos->serverTime / 1000 );
+
+        $msg = "Waktu Server: " . date('j M Y, H:i:s', $waktu) . " %0a%0a";
+
+        // Jika array low ada isinya
+        if (count($low_asset) > 0){
+            $msg .= "LOW %0a%0a";
+            for ($i=0; $i < count($low_asset); $i++) { 
+                $msg .= $i + 1 . ". " . $low_asset[$i] . " %0a";
+            }
+            $msg .= "%0a";
+        }
+        
+        // Jika array high ada isinya
+        if (count($high_asset) > 0){
+            $msg .= "HIGH %0a%0a";
+            for ($i=0; $i < count($high_asset); $i++) { 
+                $msg .= $i + 1 . ". " . $high_asset[$i] . " %0a";
+            }
+        }
+
+        // Jika salah satu dari array low dan high ada isinya maka kirim pesan
+        if (count($high_asset) > 0 || count($low_asset) > 0){
+            sendMessage($msg);
+        }
+    }
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Monitoring Binance (PHP)</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <script src="js/jquery.js"></script>
+  </head>
+  <body>
+    <!-- Header -->
+    <header class="header">
+      <h1 class="header__title">Monitoring Binance (PHP)</h1>
+    </header>
+    <!-- Akhir Header -->
+
+    <!-- Main -->
+    <main class="content">
+      <section class="tele">
+        <h2 class="tele__title">Bot Telegram</h2>
+        <div class="tele__setting">
+          <button class="tele__btn tele__btn--start">Start Bot</button>
+          <button class="tele__btn tele__btn--stop tele__btn--clicked">Stop Bot</button>
+        </div>
+        <p class="tele__detail">Bot tidak mengirimkan pesan</p>
+      </section>
+      <section class="tool">
+        <h2 class="tool__title">Marketplace Binance</h2>
+        <?php 
+          $prices = json_decode(file_get_contents('https://api.binance.me/api/v3/ticker/price'));
+
+          // limit 100 per halaman
+          $limit = 100;
+          $total_items = count($prices); // total items
+          $total_pages = ceil($total_items / $limit);
+
+          for($x = 1; $x <= $total_pages; $x++):
+            if ($x == 1) {
+              echo "<a class=\"page page--active\" href=\"binance.php?page=$x\">$x</a>";
+            } else {
+              echo "<a class=\"page\" href=\"binance.php?page=$x\">$x</a>";
+            }
+          endfor;
+        ?>
+      </section>
+      <section class="table">
+        <table>
+          <thead>
+            <tr class="table__header__row">
+              <th class="table__header table__header--number">No.</th>
+              <th class="table__header table__header--coin">Koin</th>
+              <th class="table__header table__header--pair">Pair</th>
+              <th class="table__header table__header--last">Last</th>
+              <th class="table__header table__header--low">Low</th>
+              <th class="table__header table__header--high">High</th>
+              <th class="table__header table__header--lowest">Is Lowest?</th>
+              <th class="table__header table__header--highest">Is Highest?</th>
+            </tr>
+          </thead>
+          <tbody class="table__body">
+            
+          </tbody>
+        </table>
+      </section>
+    </main>
+    <!-- Akhir Main -->
+
+    <!-- Footer -->
+    <footer class="footer">
+      <p class="footer__text">Created by Kelompok 13 API 2021</p>
+    </footer>
+    <!-- Akhir Footer -->
+
+    <script src="js/main.js"></script>
+  </body>
+</html>
